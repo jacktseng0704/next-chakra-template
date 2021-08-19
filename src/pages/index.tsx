dbf1dc002b214cf41dadb6cfeee86e3ca6217c24
Jack Tseng <b99901190@gmail.com>
2021-08-19 17:59:15 +0800

    configure prettier and eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react';
 
 export default function Home() {
-    return <Box color='blue.300'>Next.js with Chakra UI</Box>;
+  return <Box color="blue.300">Next.js with Chakra UI</Box>;
 }
